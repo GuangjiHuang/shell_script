9b0c9637d5ea203a6e60b0e92a33e330e95e1b34
GuangjiHuang <976824193@qq.com>
2021-05-18 22:17:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+*** the basic command
+p l : list the dirs
+p w : write the dirs to the *.txt
+p n : jump to the n(n is the number)
+
---
+++ path.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+record_path=/opt/MyScript/record_path.txt
+num_lines=$(sed -n '1p' $record_path)
+#echo "the numlines is : ${num_lines}"
+if [ $# -lt 1 ]
+then
+echo "Please add the args!"
+return
+fi
+case "$1" in
+l)
+# list the path in the $record_path
+cat $record_path
+;;
+w)
+# add the path to the record_path.txt
+let num_lines=$num_lines+1
+echo "${num_lines}. $(pwd)" >> $record_path
+echo "${num_lines}. $(pwd) >> $record_path"
+# renew the num_lines
+sed -i "1c${num_lines}" $record_path
+;;
+c)
+echo "0" > $record_path
+;;
+[1-9])
+let lines=$1+1
+s_path=$(sed -n "${lines}p" $record_path)
+cd_path=${s_path: 3}
+#export PATH=$PATH:${cd_path}
+export cd_path
+echo "-> ${cd_path}"
+cd ${cd_path}
+esac
