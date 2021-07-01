15173f30508ebf438b1d3290fc999de38893fc81
GuangjiHuang <GuangJiHuang@qq.com>
2021-07-01 09:15:33 +0800

    update Thu Jul  1 09:15:33 CST 2021

--- p-script/path.sh
+++ p-script/path.sh
@@ -23,7 +23,14 @@ sed -i "1c${num_lines}" $record_path
 c)
 echo "0" > $record_path
 ;;
-[1-9])
+h)
+    echo "======================================"
+    echo -e "${GREEN}OPTION${NOCOLOR}"
+    echo "======================================"
+
+    echo -e ":l->list the path you record;\nw->write the path to the rocord list;\n:c->clear all the record path;\nh->help, to show the command's option;\n:m->manmually changed the record file opened using vim;\n:[1-9]->chose the path you want to go."
+    ;;
+1-9])
 let lines=$1+1
 s_path=$(sed -n "${lines}p" $record_path)
 cd_path=${s_path: 3}
@@ -31,4 +38,7 @@ cd_path=${s_path: 3}
 export cd_path
 echo "-> ${cd_path}"
 cd ${cd_path}
+;;
+*)
+    echo "Error: you can use the command <p h> to get more information"
 esac
