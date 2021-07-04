461ef32bfeeb52686b1c597c6a0a840f24f7cb79
GuangjiHuang <GuangJiHuang@qq.com>
2021-07-04 19:40:54 +0800

    update ->2021-07-04 Sun 19:40:54

--- wr-script/write.sh
+++ wr-script/write.sh
@@ -2,6 +2,16 @@
 
 #check if there are the directory
 dir_name=$(date "+%Y-%m")/$(date "+%m-%d")
+
+#if you want to use the date-pointer's path, you have to specify the -p as the $2
+if [ "$2" == "-p" ]; then
+    dir_name=$(cat /opt/myscript/wr-script/date-pointer.txt)
+    # check if $dir_name is empty
+    if [ -z ${dir_name} ]; then
+        echo -e "->${RDD}ERROR:${NOCOLOR}the dir_name is empty! Please key ctrl+c to stop!"
+        read
+    fi
+fi
 today_path=~/mygithub/everyday_record/${dir_name}
 if [ ! -d "${today_path}" ]; then
     mkdir -p ${today_path}
@@ -26,8 +36,31 @@ case "$1" in
     "temp")
         vim ${today_path}/temp.txt
         ;;
+    "pointer-check")
+        echo -e "Point to -> $(cat /opt/myscript/wr-script/date-pointer.txt)"
+        ;;
+    "pointer")
+        dir_name=$2-$3/$3-$4
+        pointer_path=~/mygithub/everyday_record/${dir_name}
+        # check if the path exists?
+        if [ -d "${pointer_path}" ]; then
+            echo ${dir_name} > /opt/myscript/wr-script/date-pointer.txt
+            echo -e "->Successfully points to the ${dir_name}"
+        else
+            echo -e "->${RED}ERROR${NOCOLOR}: ${dir_name} not exist! Fail!"
+        fi
+        ;;
+    "help")
+        echo -e "->${GREEN}OPTION:${NOCOLOR}"
+        echo ": learn-> go to the learn.txt"
+        echo ": temp-> go the temp.txt"
+        echo ": question-> go the question.txt"
+        echo ": help-> get the help"
+        echo ": pointer year-month-day-> to set the pointer"
+        echo ": pointer-check-> to show the pointer"
+        ;;
     *)
-        echo -e "-> ${RED}ERROR:${NOCOLOR} Wrong input! You can just input the following option:\n-> ${GREEN}OPTION:${NOCOLOR} plan, learn, questiion, temp"
+        echo -e "-> ${RED}ERROR:${NOCOLOR} Wrong input! You can just input the following option:\n-> ${GREEN}OPTION:${NOCOLOR} plan, learn, question, temp, help, pointer %Y-%m-%d, pointer-check"
         ;;
 
 
