0fad89177783d2f8c9642ba780e4eaafd87aa969
GuangjiHuang <GuangJiHuang@qq.com>
2021-09-17 15:58:49 +0800

    update->2021-09-17 Fri 15:58:48

--- wr-script/write.sh
+++ wr-script/write.sh
@@ -48,10 +48,10 @@ func_goal()
             ;;
         "help")
             echo -e "${GREEN}------ HELP INFORMATION ------${NOCOLOR}"
-            echo ": list->: list the all goal"
-            echo ": create->: create the new goal"
-            echo ": help->: show the command information"
-            echo ": xxx(the goal)->: show the specification goal"
+            echo -e "${GREEN}: list->${NOCOLOR}: list the all goal"
+            echo -e "${GREEN}: create->${NOCOLOR}: create the new goal"
+            echo -e "${GREEN}: help->${NOCOLOR}: show the command information"
+            echo -e "${GREEN}: xxx(the goal)->${NOCOLOR}: show the specification goal"
             ;;
         *)
             if [ -d ${goal_dir}${arg2} ]; then
@@ -97,11 +97,15 @@ if [ ! -d "${today_path}" ]; then
         touch ${today_path}/learn.txt && echo -e "${separate_sign}\n$(datef)\n${separate_sign}\n" > ${today_path}/learn.txt
         touch ${today_path}/question.txt && echo -e "${separate_sign}\n$(datef)\n${separate_sign}\n" > ${today_path}/question.txt
         touch ${today_path}/temp.txt && echo -e "${separate_sign}\n$(datef)\n${separate_sign}\n" > ${today_path}/temp.txt
+        touch ${today_path}/type.pratice && echo -e "${separate_sign}\n$(datef)\n${separate_sign}\n" > ${today_path}/type.pratice
         # add the todolist.txt (copy the template to it );
         separate_sign_2="--------------------------------------"
         touch ${today_path}/todolist.txt && echo -e "${separate_sign_2}\n------ $(datef) -------\n${separate_sign_2}" > ${today_path}/todolist.txt
         cat /opt/myscript/wr-script/todolist/todolist.template >> ${today_path}/todolist.txt
         echo "${today_path}/todolist.txt" > /opt/myscript/wr-script/todolist/date-path.txt
+        # copy the type.template to the type.pratice
+        cat /opt/myscript/wr-script/type.template >> ${today_path}/type.pratice
+        echo $separate_sign_2 >> ${today_path}/type.pratice
     fi
 
 fi
@@ -213,30 +217,84 @@ case "$1" in
     "goal")
         func_goal $1 $2 $3
         ;;
+    "type")
+        if [ -z $2 ]; then
+             #open the type.pratice
+             vim ${today_path}/type.pratice
+        elif [ "$2" == "m" ]; then
+             #open the type.template
+             vim /opt/myscript/wr-script/type.template
+        elif [ "$2" == "c" ]; then
+             # copy the /opt/myscirpt/wr-script/type.template to the ~/mygithub/shell_script/wr-script/type.tempate
+             cp /opt/myscript/wr-script/type.template ~/mygithub/shell_script/wr-script/type.template
+             echo ":->Successfully renew the type.template!"
+         elif [ "$2" == "renew" ]; then
+             # copy the type.template to the type.pratice, because the type.tempate has been renew
+             echo -e "${separate_sign}\n$(datef)\n${separate_sign}\n" > ${today_path}/type.pratice
+             cat /opt/myscript/wr-script/type.template >> ${today_path}/type.pratice
+             echo $separate_sign_2 >> ${today_path}/type.pratice
+             echo ":->Has been renew the ${today_path}/type.pratice"
+             # if you want to open it, add the $3
+             if [ "$3" == "o" ]; then
+                 vim ${today_path}/type.pratice
+             fi
+        else
+             echo -e "Error: no the command: \" wr type $2 \""
+        fi
+        ;;
     "help")
-        echo -e "->${GREEN}OPTION:${NOCOLOR}"
-        echo ": love->: my dear sq"
-        echo ": learn->: go to the learn.txt"
-        echo ": temp->: go the temp.txt"
-        echo ": question->: go the question.txt"
-        echo ": help->: get the help"
-        echo ": pointer->: year-month-day-> to set the pointer"
-        echo ": pointer-check->: to show the pointer"
-        echo ": go-e->: cd to the everyday_record"
-        echo ": go-wr->: cd to the write-scritp.sh"
-        echo ": go-goal->: cd to the goal directroy"
-        echo ": merge->: merge the today's question, plan to the compress's directory"
-        echo ": all->: use .../compress/, you can edit the compress's file"
-        echo ": rest->: show the rest as the screen"
-        echo ": moyu->: show the moyu as the screen"
-        echo ": xiaban->: off duty"
-        echo ": todo->: edit the todolist.txt"
-        echo ": todo-cfg->: modify the todo configuration"
-        echo ": todo-exe->: execute the prgrom to read, count, and then write"
-        echo ": goal xxx: about the goal, you can use the command < wr goal help > for more information"
+        clear
+        echo -e "${YELLOW}-------------------------------> OPTION <---------------------------------------${NOCOLOR}"
+        #echo -e "--------------------${GREEN}OPTION${NOCOLOR}--------------------"
+
+        echo -e "${GREEN}: all->:${NOCOLOR} use .../compress/, you can edit the compress's file"
+        echo
+        echo -e "${GREEN}: go-e->${NOCOLOR}: cd to the everyday_record"
+        echo 
+        echo -e "${GREEN}: go-wr->${NOCOLOR}: cd to the write-scritp.sh"
+        echo 
+        echo -e "${GREEN}: go-goal->${NOCOLOR}: cd to the goal directroy"
+        echo 
+        echo -e "${GREEN}: goal xxx.txt${NOCOLOR}: about the goal, you can use the command < wr goal help > for more information"
+        echo 
+        echo -e "${GREEN}: help->${NOCOLOR}: get the help"
+        echo 
+        echo -e "${GREEN}: learn->${NOCOLOR}: go to the learn.txt"
+        echo 
+        echo -e "${GREEN}: love->${NOCOLOR}: my dear sq"
+        echo 
+        echo -e "${GREEN}: merge->${NOCOLOR}: merge the today's question, plan to the compress's directory"
+        echo 
+        echo -e "${GREEN}: moyu->${NOCOLOR}: show the moyu as the screen"
+        echo 
+        echo -e "${GREEN}: pointer-check->${NOCOLOR}: to show the pointer"
+        echo 
+        echo -e "${GREEN}: pointer->${NOCOLOR}: year-month-day-> to set the pointer"
+        echo 
+        echo -e "${GREEN}: question->${NOCOLOR}: go the question.txt"
+        echo 
+        echo -e "${GREEN}: rest->${NOCOLOR}: show the rest as the screen"
+        echo 
+        echo -e "${GREEN}: temp->${NOCOLOR}: go the temp.txt"
+        echo 
+        echo -e "${GREEN}: todo->${NOCOLOR}: edit the todolist.txt"
+        echo 
+        echo -e "${GREEN}: todo-cfg->${NOCOLOR}: modify the todo configuration"
+        echo 
+        echo -e "${GREEN}: todo-exe->${NOCOLOR}: execute the prgrom to read, count, and then write"
+        echo 
+        echo -e -e "${GREEN}: type x y->${NOCOLOR}: 1) no x: open the type.pratice; 2) x=m: change the type.template manually;\n"\
+        "             3) x=c: copy the /opt/.../type.template to the ~/mygithub/.../type.template;\n"\
+        "             4)x=renew: renew the today's type.pratice, if the y=o, it will open the type.pratice"
+        echo 
+        echo -e "${GREEN}: xiaban->${NOCOLOR}: Upload the file to the github reposity, and then off duty"
+        echo 
+        echo -e "${GREEN}: wr->${NOCOLOR}:modify the write.sh"
+        echo 
+        echo -e "${YELLOW}--------------------------------------------------------------------------------${NOCOLOR}"
         ;;
     *)
-        echo -e "-> ${RED}ERROR:${NOCOLOR} Wrong input! You can just input the following option:\n-> ${GREEN}OPTION:${NOCOLOR} love, plan, learn, question, temp, help, pointer %Y-%m-%d, pointer-check, go-e, go-wr, review, all, rest, todo, todo-cfg, todo-exe, ... and so on "
+        echo -e "-> ${RED}ERROR:${NOCOLOR} Wrong input! You can just input the following option:\n-> ${GREEN}OPTION:${NOCOLOR} love, plan, learn, question, temp, help, pointer %Y-%m-%d, pointer-check, go-e, go-wr, review, all, rest, todo, todo-cfg, todo-exe, type x... and so on "
         echo -e "${GREEN}You can use the wr help for more information!${NOCOLOR}"
         ;;
 esac
