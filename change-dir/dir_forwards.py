6883c4a121a2d0f40e01f8e00a076c7b5ad32a51
GuangjiHuang <976824193qq.com>
2023-02-17 20:46:59 +0800

    update->2023-02-17 周五 20:46:59

--- change-dir/dir_forwards.py
+++ change-dir/dir_forwards.py
@@ -2,26 +2,31 @@
 import os
 import re
 import sys
+import time
 
+t_s = time.time()
 args = sys.argv[1: ]
 if not args:
-    print(".")
+    print(f".#{time.time()-t_s:.1f}s")
     exit(0)
 
 # get all the dir path
+#
 for root, dirs, files in os.walk("."):
     # check the root
     is_meet_flag = True
-    root_and_dir_ls = [root] + [root+"/"+i for i in dirs]
+    #root_and_dir_ls = [root] + [root+"/"+i for i in dirs]
+    root_and_dir_ls = [root]
     for root_and_dir in root_and_dir_ls:
         is_root_and_dir_meet_need = True
         for arg in args:
-            if arg not in root_and_dir:
+            if arg not in root_and_dir.lower():
                 is_root_and_dir_meet_need = False
                 break
         # check
         if is_root_and_dir_meet_need:
-            print(root_and_dir)
+            print(f"{root_and_dir}#{time.time()-t_s:.1f}s")
             exit(0)
+        # 
 # can not search
-print(".")
+print(f".#{time.time()-t_s:.1f}s")
--- change-dir/dir_forwards.sh
+++ change-dir/dir_forwards.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-forward_path=$(python /opt/myscript/change-dir/dir_forwards.py $@)
-echo "-> ${forward_path}"
-cd $forward_path
+get_string=$(python /opt/myscript/change-dir/dir_forwards.py $@)
+time_usage=${get_string#*#}
+forward_path=${get_string%#*}
+echo "-> ${forward_path} [$time_usage]"
+cd "$forward_path"
